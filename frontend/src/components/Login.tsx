128a69ba58ddc3bfb354ce5b374f26cfb073518a
Dominik Fucic <dominikfucic90@gmail.com>
2023-06-14 20:59:25 +0200

    Created <Login /> and <Register /> components.

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
+import {
+  Anchor,
+  Group,
+  Text,
+  PasswordInput,
+  Container,
+  Button,
+  Title,
+  TextInput,
+} from "@mantine/core";
 
 export default function Login() {
   const auth = useAuth();
@@ -8,8 +18,40 @@ export default function Login() {
 
   if (auth?.user) return <Navigate to="/" replace />;
 
-  return <div>
-    <h1>Login</h1>
-    <button onClick={() => navigate("/register")}>Register</button>
-  </div>;
+  return (
+    <Container my="xl">
+      <Title order={1} mb="xl">
+        Log in
+      </Title>
+
+      <TextInput placeholder="Your name" label="Full name" mb="sm" />
+
+      <Group position="apart" mb={5}>
+        <Text component="label" htmlFor="your-password" size="sm" weight={500}>
+          Your password
+        </Text>
+
+        <Anchor<"a">
+          href="#"
+          onClick={(event) => event.preventDefault()}
+          sx={(theme) => ({
+            paddingTop: 2,
+            color:
+              theme.colors[theme.primaryColor][
+                theme.colorScheme === "dark" ? 4 : 6
+              ],
+            fontWeight: 500,
+            fontSize: theme.fontSizes.xs,
+          })}
+        >
+          Forgot your password?
+        </Anchor>
+      </Group>
+      <PasswordInput placeholder="Your password" id="your-password" mb="sm" />
+      <Group position="left">
+        <Button onClick={() => navigate("/")}>Log in</Button>
+        <Button onClick={() => navigate("/register")}>Register</Button>
+      </Group>
+    </Container>
+  );
 }
--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { useNavigate, Navigate } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
+import {
+  Anchor,
+  Group,
+  Text,
+  PasswordInput,
+  Container,
+  Button,
+  Title,
+  TextInput,
+  Flex,
+} from "@mantine/core";
+import { IconAt } from "@tabler/icons-react";
 
 export default function Register() {
   const auth = useAuth();
@@ -9,9 +21,25 @@ export default function Register() {
   if (auth?.user) return <Navigate to="/" replace />;
 
   return (
-    <div>
-      <h1>Register</h1>
-      <button onClick={() => navigate("/login")}>Back to login</button>
-    </div>
+    <Container my="xl">
+      <Title order={1} mb="xl">
+        Register
+      </Title>
+
+      <Flex direction="column" gap="sm">
+        <TextInput placeholder="your name" label="Full name" />
+        <TextInput
+          label="Email"
+          placeholder="user@mail.com"
+          icon={<IconAt size="0.8rem" />}
+        />
+        <PasswordInput label="Password" id="your-password" />
+        <PasswordInput label="Repeat password" id="repeat-password" mb="sm" />
+      </Flex>
+      <Group position="left">
+        <Button onClick={() => navigate("/login")}>Log In</Button>
+        <Button onClick={() => navigate("/")}>Register</Button>
+      </Group>
+    </Container>
   );
 }
